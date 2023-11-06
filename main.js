9c4144aea1f27be8fe8e380ce96096bb167cff7e
Coco <coulet.corentin@gmail.com>
2023-11-06 14:57:43 +0100

    exo6

--- main.js
+++ main.js
@@ -1,106 +1,45 @@
-/* 
-// Étape 1
-class Clock extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Bonjour, monde !</h1>
-          <h2>Il est {this.props.date.toLocaleTimeString()}.</h2>
-        </div>
-      );
-    }
-  }
-
-ReactDOM.render(<Clock date={new Date()}/>, document.querySelector('#app')); 
-
-*/
-
-
-/* 
-
-// Étape 2
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date()};
-    }
-  render() {
-    return (
-      <div>
-        <h1>Bonjour, monde !</h1>
-        <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app')); 
-
-
- */
-
-
-
-// Étape 3
-
-/* class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date()};
-    }
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(),1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        })
-    }
-
-    render() {
-        return (
-        <div>
-            <h1>Bonjour, monde !</h1>
-            <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-        );
-    }
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app'));  */
-
-
-function Clock(props) {
-    React.useEffect(() => {
-        tick();        
-    }, []);
-    
+function Clock() {
     const [date, setDate] = React.useState(new Date());
+    const [textColor, setTextColor] = React.useState("black");
+    const timerID = React.useRef(null);
 
-    const tick = () => {
-        setInterval(() => {
-            setDate(new Date())
-        }, 1000)
-    }
-    
+    React.useEffect(() => {
+        startClock();
+        return function cleanup() {
+            stopClock();
+        };
+    }, []);
+
+    const startClock = () => {
+        timerID.current = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+    };
+
+    const stopClock = () => {
+        clearInterval(timerID.current);
+        timerID.current = null;
+    };
+
+    const handleRandomColor = () => {
+        const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        setTextColor(randomColor);
+    };
+
+    const handleResetColor = () => {
+        setTextColor("black");
+    };
 
     return (
         <div>
             <h1>Hello world</h1>
-            <h2>Il est {date.toLocaleTimeString()}.</h2>
+            <h2 style={{ color: textColor }}>Il est {date.toLocaleTimeString()}.</h2>
+            <button onClick={handleRandomColor}>Changer Couleur</button>
+            <button onClick={handleResetColor}>Réinitialiser Couleur</button>
+            <button onClick={stopClock}>Arrêter Horloge</button>
+            <button onClick={() => { startClock(); handleResetColor(); }}>Re-synchroniser Horloge</button>
         </div>
-        );
+    );
 }
 
 ReactDOM.render(<Clock />, document.querySelector('#app'));
-
-
-
-
-
